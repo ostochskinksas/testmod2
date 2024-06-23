c2dc6e622efdccb78276e95061d9b729eb52452c
ostochskinksas <lucaslarsson2003@gmail.com>
2024-06-23 18:28:56 +0200

    test mod 2 init

---
+++ src/client/java/ostochskinksas/testmod2/Testmod2Client.java
@@ -0,0 +1,10 @@
+package ostochskinksas.testmod2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Testmod2Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/ostochskinksas/testmod2/Testmod2.java
@@ -0,0 +1,23 @@
+package ostochskinksas.testmod2;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Testmod2 implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "testmod2";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
